15d8079ce2990d0248f8aa4488e611a09d21fb7b
Liam Watson <Liam_watson2003@hotmail.com>
2017-05-02 04:52:17 +1000

    initial upload of files

---
+++ Assets/Cloth.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloth : MonoBehaviour {
+
+    public GameObject[] otherGO;
+
+    public float maxdistance;
+    public float gravpower;
+
+    private float mag;
+
+    public float correctionForce;
+    public float force;
+
+    public float StretchThreshold;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < 5; i++)
+        {
+            foreach (GameObject GO in otherGO)
+            {
+
+                if (GO != null)
+                {
+                    Debug.DrawLine(this.transform.position, GO.transform.position, Color.blue);
+                    mag = wobkmath.Instance.GetMag(transform, GO.transform);
+
+                    correctionForce = force / 10 * mag / 2;
+                    //Debug.Log(mag + " and " + maxdistance);
+                    if (mag > maxdistance)
+                    {
+                        wobkmath.Instance.MoveTowards(this.gameObject.transform, GO.transform, correctionForce, false, true);
+                    }
+
+                    if (mag < maxdistance + StretchThreshold)
+                    {
+                        transform.Translate(Vector3.down * (gravpower * Time.deltaTime) / otherGO.Length);
+                    }
+                }
+            }//end foreach
+        }
+        //transform.Translate(Vector3.down * gravpower * Time.deltaTime);
+
+
+
+
+    }
+}
---
+++ Assets/wobkmath.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wobkmath : MonoBehaviour {
+
+    //Create the Singleton
+    public static wobkmath Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+
+    //wobkmath 
+    //This is my scripting API used to shorten math required by games
+    #region Vector 3 Math
+    //Magnitude Function, takes 2 transforms and returns a float of distance
+    public float GetMag(Transform obj1, Transform obj2)
+    {
+        Vector3 VectorDifference = obj1.position - obj2.position;
+        float Distance = VectorDifference.sqrMagnitude;
+        return Distance;
+    }
+    public Transform MoveTowards(Transform obj1, Transform obj2, float Amount, bool MoveAwayFrom, bool KeepRotation)
+    {
+        //used to rotate the object back to its original rotation
+        Quaternion old = obj1.transform.rotation;
+
+        if (!MoveAwayFrom)
+        {
+            obj1.transform.LookAt(obj2);
+            obj1.transform.Translate(Vector3.forward * Amount * Time.deltaTime);
+            if (KeepRotation)
+                obj1.rotation = old;
+            return obj1.transform;
+        }
+        else
+        {
+            obj1.transform.LookAt(obj2);
+            obj1.transform.Rotate(0, 180, 0);
+            obj1.transform.Translate(Vector3.forward * Amount * Time.deltaTime);
+            obj1.transform.Rotate(0, 180, 0);
+            if (KeepRotation)
+                obj1.rotation = old;
+            return obj1.transform;
+        }
+    }
+
+    #endregion
+
+}
